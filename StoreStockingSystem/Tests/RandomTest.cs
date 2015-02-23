9fb9cc9ebed82caee34be014eb7e8da6f55518af
fboja <filipboja@gmail.com>
2015-02-23 15:51:26 +0100

    Humble beginnings

---
+++ StoreStockingSystem/Tests/RandomTest.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace StoreStockingSystem.Tests
+{
+    [TestFixture]
+    public class RandomTest
+    {
+
+        [Test]
+        public void Hello()
+        {
+            Assert.AreEqual(2, 2);
+        }
+        
+    }
+}
