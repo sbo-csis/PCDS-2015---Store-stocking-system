dbdde02488774e9949ef90d46453f28a11386a74
fboja <filipboja@gmail.com>
2015-03-02 16:21:02 +0100

    Added store performance mockup and changed top menu layout.

---
+++ compare/Controllers/StorePerformanceController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PCDSWebsite.Controllers
+{
+    public class StorePerformanceController : Controller
+    {
+        // GET: StorePerformance
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
