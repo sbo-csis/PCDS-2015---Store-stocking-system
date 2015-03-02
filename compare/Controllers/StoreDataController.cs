264fe4c00c81e26cfcaa1b1c8e4dd7ed82260c52
fboja <filipboja@gmail.com>
2015-03-02 13:09:50 +0100

    Changed namespace of mock project and added datatables jquery library.

---
+++ compare/Controllers/StoreDataController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+using System.Data;
+
+namespace PCDSWebsite.Controllers
+{
+    public class StoreDataController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View("StoreDataList");
+        }
+
+        public static DataTable MakeMockTable()
+        {
+            
+        }
+    }
+}
--- compare/Global.asax
+++ compare/Global.asax
@@ -1,1 +1,1 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="compare.MvcApplication" Language="C#" %>
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PCDSWebsite.MvcApplication" Language="C#" %>
