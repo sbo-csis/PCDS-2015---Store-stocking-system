b11c2d5ae30839f17ca50c35d05ca486f95923d5
fboja <filipboja@gmail.com>
2015-03-09 17:06:10 +0100

    Added displaytype and store services.

--- StoreStockingSystem/Services/StockService.cs
+++ StoreStockingSystem/Services/StockService.cs
@@ -6,7 +6,25 @@ namespace StoreStockingSystem.Services
     {
         public static void NewStock(Stock newStock)
         {
-            
+            using (var context = new StoreStockingContext())
+            {
+                context.Stocks.Add(newStock);
+                context.SaveChanges();
+            }
+        }
+
+        public static void NewStock(int storeId, int displayTypeId, int? capacity, int? warningPercentage)
+        {
+            var store = StoreService.GetStore(storeId);
+            var displayType = DisplayTypeService.GetDisplayType(displayTypeId);
+
+            NewStock(new Stock
+            {
+                Capacity = capacity ?? 9999,
+                DisplayType = displayType,
+                Store = store,
+                WarningPercentageStockLeft = warningPercentage ?? 0
+            });
         }
 
         public static void AddStock(int storeId, int productId, int amount)
--- StoreStockingSystem/Services/StoreService.cs
+++ StoreStockingSystem/Services/StoreService.cs
@@ -5,6 +5,18 @@ namespace StoreStockingSystem.Services
 {
     public static class StoreService
     {
+        public static Store GetStore(int storeId)
+        {
+            using (var context = new StoreStockingContext())
+            {
+                var store = (from t in context.Stores
+                             where t.Id == storeId
+                             select t).FirstOrDefault();
+
+                return store;
+            }
+        }
+
         public static void GetStoreData(string storeName)
         {
             using (StoreStockingContext ssc = new StoreStockingContext())
