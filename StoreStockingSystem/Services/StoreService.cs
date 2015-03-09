0933a3ab4257c49e61673d0bfd55436ac7008c19
sbo-csis <sbo@csis.dk>
2015-03-09 16:29:58 +0100

    Namespace update

--- StoreStockingSystem/Services/StoreService.cs
+++ StoreStockingSystem/Services/StoreService.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Linq;
 using StoreStockingSystem.Models;
 
-namespace StoreStockingSystem.Controller
+namespace StoreStockingSystem.Services
 {
     public static class StoreService
     {
