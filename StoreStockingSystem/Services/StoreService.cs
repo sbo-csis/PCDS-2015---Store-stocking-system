14cfa8a948c90b3dbd7be314663ffcf962feefa6
sbo-csis <sbo@csis.dk>
2015-03-09 17:17:09 +0100

    Filip is too fast

--- StoreStockingSystem/Services/StoreService.cs
+++ StoreStockingSystem/Services/StoreService.cs
@@ -20,24 +20,5 @@ namespace StoreStockingSystem.Services
                 return store;
             }
         }
-
-        public static Stock GetStoreData(string storeName)
-        {
-            using (StoreStockingContext ssc = new StoreStockingContext())
-            {
-                var storeData = ssc.Stocks.Select(y => y).Where(z => z.Store.Name == storeName);
-                return storeData.FirstOrDefault();
-            }
-        }
-
-        public static List<Sale> GetSalesData(DateTime after, DateTime before)
-        {
-            using (StoreStockingContext ssc = new StoreStockingContext())
-            {
-                var storeData = from t in ssc.Sales where DateTime.Compare(t.SalesDate, after) > 0 && DateTime.Compare(t.SalesDate, before) < 0 select t;
-                //ssc.Sales.SelectMany(t => t).Where(y => DateTime.Compare(y.SalesDate, after) > 0 && DateTime.Compare(y.SalesDate, before) < 0);
-                //return storeData.SelectMany(sale => sale.);
-            }
-        }
     }
 }
