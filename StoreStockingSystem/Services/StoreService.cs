7d8208f58f8c3de4df3380d282e3171e9b78fff9
sbo-csis <sbo@csis.dk>
2015-03-09 17:08:35 +0100

    First draft on getters

--- StoreStockingSystem/Services/StoreService.cs
+++ StoreStockingSystem/Services/StoreService.cs
@@ -1,15 +1,30 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net.Sockets;
 using StoreStockingSystem.Models;
 
 namespace StoreStockingSystem.Services
 {
     public static class StoreService
     {
-        public static void GetStoreData(string storeName)
+        public static Stock GetStoreData(string storeName)
         {
             using (StoreStockingContext ssc = new StoreStockingContext())
             {
-                var x = ssc.ProductStocks.Select(y => y.Stock).Where(z => z.Store.Name == storeName);
+                var storeData = ssc.Stocks.Select(y => y).Where(z => z.Store.Name == storeName);
+                return storeData.FirstOrDefault();
+            }
+        }
+
+        public static List<Sale> GetSalesData(DateTime after, DateTime before)
+        {
+            using (StoreStockingContext ssc = new StoreStockingContext())
+            {
+                var storeData = from t in ssc.Sales where DateTime.Compare(t.SalesDate, after) > 0 && DateTime.Compare(t.SalesDate, before) < 0 select t;
+                //ssc.Sales.SelectMany(t => t).Where(y => DateTime.Compare(y.SalesDate, after) > 0 && DateTime.Compare(y.SalesDate, before) < 0);
+                //return storeData.SelectMany(sale => sale.);
             }
         }
     }
