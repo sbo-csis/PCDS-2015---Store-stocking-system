5b79f5e3172dc176271cbfb1d05f3673f8acd51e
sbo-csis <sbo@csis.dk>
2015-03-09 16:25:13 +0100

    Redid structure a bit

---
+++ StoreStockingSystem/Services/StoreService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using StoreStockingSystem.Models;
+
+namespace StoreStockingSystem.Controller
+{
+    public static class StoreService
+    {
+        public static void GetStoreData(string storeName)
+        {
+            using (StoreStockingContext ssc = new StoreStockingContext())
+                var x = ssc.ProductStocks.SelectMany(y => y.Stock).Where(z => z.Store.Name == storeName);
+        }
+        }
+    }
+}
