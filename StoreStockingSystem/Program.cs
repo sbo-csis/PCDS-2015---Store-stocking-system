f29211ef7e96fe72fcb46218217901d09b4ca22c
sbo-csis <sbo@csis.dk>
2015-03-09 16:12:46 +0100

    Skeleton code for SSS

---
+++ StoreStockingSystem/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StoreStockingSystem
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            
+        }
+    }
+}
