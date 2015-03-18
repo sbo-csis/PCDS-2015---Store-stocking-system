54f47f1aa5ca30cdcc14a2b79d5ad1fd8f43d3c7
DGandrup <davidgandrup@gmail.com>
2015-03-18 12:30:08 +0100

    Added StoreServiceTest

---
+++ StoreStockingSystem.Test/Tests/StoreServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using StoreStockingSystem.Models;
+
+namespace StoreStockingSystem.Test.Tests
+{
+    [TestFixture]
+    public class StoreServiceTest
+    {
+        [Test]
+        public void make_and_delete_store()
+        {
+            // TODO: 
+        }
+    }
+}
