0efff634a2b5aad12ed85d7a11aaeb4c488995c9
fboja <filipboja@gmail.com>
2015-03-18 13:16:49 +0100

    Finished basic product service.

--- StoreStockingSystem/Services/ProductService.cs
+++ StoreStockingSystem/Services/ProductService.cs
@@ -1,9 +1,19 @@
-﻿using StoreStockingSystem.Models;
+﻿using System;
+using System.Linq;
+using StoreStockingSystem.Models;
 
 namespace StoreStockingSystem.Services
 {
     public static class ProductService
     {
+        public static Product GetProduct(int productId)
+        {
+            using (var context = new StoreStockingContext())
+            {
+                return context.Products.Find(productId);
+            }
+        }
+
         public static int NewProduct(Product product)
         {
             using (var context = new StoreStockingContext())
@@ -17,6 +27,26 @@ namespace StoreStockingSystem.Services
         public static int NewProduct(string name, int price)
         {
             return NewProduct(new Product() {Name = name, Price = price});
+        }
+
+        public static void UpdateProduct(Product changedProduct)
+        {
+            using (var context = new StoreStockingContext())
+            {
+                var currentProduct = (from t in context.Products
+                                      where t.Id == changedProduct.Id
+                                      select t).FirstOrDefault();
+
+                if (currentProduct != null)
+                {
+                    currentProduct = changedProduct;
+                    context.SaveChanges();
+                }
+                else
+                {
+                    throw new ArgumentException("Could not update product. Product id " + changedProduct.Id + " not found.");
+                }
+            }
         }
     }
 }
