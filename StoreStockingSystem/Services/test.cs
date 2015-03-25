364084ef13a3514e947410072fea1ab2d138f905
Simon Boje Outzen <sbo@csis.dk>
2015-03-25 22:05:55 +0100

    Added test data to show the 3 product statuses

--- StoreStockingSystem/Services/test.cs
+++ StoreStockingSystem/Services/test.cs
@@ -12,15 +12,27 @@ namespace StoreStockingSystem.Services
         {
             SalesPerson sp = new SalesPerson() {Name = "Person1"};
             DisplayType dt = new DisplayType() {Name = "display1" };
-            Store store = new Store() { Name = "Store1", SalesPerson = sp};
-            Stock stock = new Stock() { Store = store, Capacity = 20, DisplayType = dt };
-            for (int i = 0; i < 100; i++)
+            Store store1 = new Store() { Name = "Store1", SalesPerson = sp};
+            Store store2 = new Store() { Name = "Store2", SalesPerson = sp };
+            Store store3 = new Store() { Name = "Store3", SalesPerson = sp };
+            Stock stock1 = new Stock() { Store = store1, Capacity = 20, DisplayType = dt };
+            Stock stock2 = new Stock() { Store = store2, Capacity = 20, DisplayType = dt };
+            Stock stock3 = new Stock() { Store = store3, Capacity = 20, DisplayType = dt };
+            for (int i = 0; i < 20; i++)
             {
                 Product p = new Product() { Name = String.Format("{0}", "Product" + i), Price = i};
-                ProductStock productStock = new ProductStock() {Stock = stock, Amount = 10, WarningAmount = 10, Product = p};
-                stock.ProductStock.Add(productStock);
+                ProductStock productStock1 = new ProductStock() {Stock = stock1, Amount = 10, WarningAmount = 10, Product = p};
+                ProductStock productStock2 = new ProductStock() { Stock = stock2, Amount = 15, WarningAmount = 10, Product = p };
+                ProductStock productStock3 = new ProductStock() { Stock = stock3, Amount = 20, WarningAmount = 10, Product = p };
+                stock1.ProductStock.Add(productStock1);
+                stock2.ProductStock.Add(productStock2);
+                stock3.ProductStock.Add(productStock3);
             }
-            ssc.Stocks.Add(stock);
+
+            
+            ssc.Stocks.Add(stock1);
+            ssc.Stocks.Add(stock2);
+            ssc.Stocks.Add(stock3);
             ssc.SaveChanges();
         }
     }
