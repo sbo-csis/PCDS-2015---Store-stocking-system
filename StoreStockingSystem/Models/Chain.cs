ec9592c98d70e6ae0783277e32504d2bad43111c
fb@godream.com <fb@GD-CLIENT-FB.godream.local>
2015-04-20 15:09:01 +0200

    Fixing merge

--- StoreStockingSystem/Models/Chain.cs
+++ StoreStockingSystem/Models/Chain.cs
@@ -8,11 +8,7 @@ namespace StoreStockingSystem.Models
         [Key]
         public int Id { get; set; }
         public string Name { get; set; }
-<<<<<<< HEAD
         public string ExternalId { get; set; }
-=======
-
         public virtual List<Store> ChainStores { get; set; }
->>>>>>> ae36a382139fe1143680a2912c99c9c3e1553486
     }
 }
